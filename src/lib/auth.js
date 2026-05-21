ce67d8c1998d49b492399db001985fff7f2ff80a
Easin Arafat <arafatgw5@gmail.com>
2026-05-21 11:39:18 +0600

    Done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,7 +8,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
-const client = new MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/tutors-finder");
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
